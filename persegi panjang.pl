9507a07f5162c570158507b94f65deec894346a6
muktics <ardanamukti@gmail.com>
2019-02-19 15:34:50 +0800

    menambahkan persegi panjang

---
+++ persegi panjang.pl
@@ -0,0 +1,5 @@
+program:-nl,
+    write('Masukkan Panjang : '),read(Panjang),
+    write('Masukkan Lebar : '),read(Lebar),
+    Luas is (Panjang*Lebar),
+    write('Luas Persegi Panjang : '),write(Luas).
